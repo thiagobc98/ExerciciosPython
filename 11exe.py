f25a3257c61414249c4c1fa580aadedc42241948
thiagobc98 <64330073+thiagobc98@users.noreply.github.com>
2021-11-01 11:44:41 -0300

    Primeiro Commit
    Exercício da lista 11 de ADSTI

---
+++ 11exe.py
@@ -0,0 +1,45 @@
+escola = {
+"manha" : {
+ "aluno1": {
+ "nome": "Clara",
+ "disciplina": "Redes"
+ },
+ "aluno2": {
+ "nome": "Jonas",
+ "disciplina": "Banco de Dados"
+ },
+ "aluno3": {
+ "nome": "Mara",
+ "disciplina": "Algoritmos"
+ }
+},
+"tarde" : {
+ "aluno1": {
+ "nome": "Ana",
+ "disciplina": "Redes"
+ },
+ "aluno2": {
+ "nome": "Rafael",
+ "disciplina": "Banco de Dados"
+ }
+},
+"noite":{
+ "aluno1": {
+ "nome": "Carla",
+ "disciplina": "Redes"
+ },
+ "aluno2": {
+ "nome": "Lara",
+ "disciplina": "Banco de Dados"
+ }
+}
+}
+print(escola)
+
+
+
+
+
+
+
+
